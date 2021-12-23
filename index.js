edb7e8829c6f53f1aac56290a727a6bf8fa0193e
Abdulloh007 <sabirovabdulloh2004@gmail.com>
2021-12-23 20:52:20 +0300

    v1.5.

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ window.addEventListener('DOMContentLoaded', function () {
     'use strict';
     // Header
     // Mobile Menu Srcipt
-    // const sideBar = document.querySelector('.nl-m-menu'),
-    // sideBarOverlay = document.querySelector('.nl-menu_overlay'),
-    // sideBarBtn = document.querySelector('.nl-humburger');
-    //
-    // sideBarBtn.addEventListener('click', function () {
-    //     if (this.classList.contains('active')) {
-    //         this.classList.remove('active');
-    //         sideBar.classList.remove('open');
-    //         sideBarOverlay.classList.remove('open');
-    //         sideBarOverlay.style.display = 'none'
-    //         document.documentElement.style.overflow = ''
-    //     }
-    //     else {
-    //         this.classList.add('active');
-    //         sideBar.classList.add('open');
-    //         sideBarOverlay.classList.add('open');
-    //         sideBarOverlay.style.display = '';
-    //         document.documentElement.style.overflow = 'hidden'
-    //     }
-    // });
+    const sideBar = document.querySelector('.nl-side-menu'),
+    sideBarOverlay = document.querySelector('.nl-menu_overlay'),
+    sideBarBtn = document.querySelector('.nl-humburger');
+
+    sideBarBtn.addEventListener('click', function () {
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+            sideBar.classList.remove('open');
+            sideBarOverlay.classList.remove('open');
+            sideBarOverlay.style.display = 'none'
+            document.documentElement.style.overflow = ''
+        }
+        else {
+            this.classList.add('active');
+            sideBar.classList.add('open');
+            sideBarOverlay.classList.add('open');
+            sideBarOverlay.style.display = '';
+            document.documentElement.style.overflow = 'hidden'
+        }
+    });
 
     //ScrollTop Fixed menu
     const nav = document.querySelectorAll('.nl-nav');
